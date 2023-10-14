18308b31b181f466da1e5646a5940382f37c7e15
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-10-14 12:19:32 +0100

    Updated main class

--- TP3/1_Gestion_Biblio/main.java
+++ TP3/1_Gestion_Biblio/main.java
@@ -1,6 +1,26 @@
 public class main {
-    public static void main(String[] args) {
-        Personne p1 = new Personne("El Mabrouki", "Ilyas","ilyas@gmail.com","0777402279",21);
-        System.out.println(p1.toString());
-    }
+
+  public static void main(String[] args) {
+    Adherant ad1 = new Adherant(
+      7,
+      "El Mabrouki",
+      "Ilyas",
+      "ilyas@gmail.com",
+      "0777402279",
+      21
+    );
+    Livre livre1 = new Livre(
+      23,
+      new Auteur(
+        10,
+        "Paulo",
+        "Coelho",
+        "paulofmcoelho@gmail.com",
+        "00351937888888",
+        76
+      )
+    );
+    System.out.println(ad1.toString());
+    System.out.println(livre1.toString());
+  }
 }
