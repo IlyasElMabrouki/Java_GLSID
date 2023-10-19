57bb226d7071b0bfdeeb3f9bae02757f4b1486a1
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-10-19 14:18:12 +0100

    Added Ingenieur Class

---
+++ TP3/2_Gestion_Salaire/Ingenieur.java
@@ -0,0 +1,21 @@
+package Exercice2;
+
+public class Ingenieur extends Employe{
+
+    private String specialite;
+
+    public Ingenieur(String nom, String prenom, String email, String telephone, double salaire, String specialite) {
+        super(nom,prenom,email,telephone,salaire);
+        this.specialite = specialite;
+    }
+
+    @Override
+    public double calculerSalaire() {
+        return super.getSalaire() + super.getSalaire() * 0.15;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nIngénieur: spécialité:" + this.specialite;
+    }
+}
