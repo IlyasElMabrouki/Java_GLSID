33b72b5a65370399f195cc88d4d4e748c2daa44a
IlyasElMabrouki <ilyaselmabrouki7@gmail.com>
2023-10-19 14:18:55 +0100

    Added Main Class

---
+++ TP3/2_Gestion_Salaire/Main.java
@@ -0,0 +1,14 @@
+package Exercice2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ingenieur ing1 = new Ingenieur("El Mabrouki", "Ilyas", "ilyas@gmail.com","0777402279",10000,"IT");
+        System.out.println(ing1.toString());
+        System.out.println(ing1.calculerSalaire());
+
+        Manager man1 = new Manager("Musk", "Elon", "musk@mail.com","0030829839",20000,"IT");
+        System.out.println(man1.toString());
+        System.out.println(man1.calculerSalaire());
+    }
+}
